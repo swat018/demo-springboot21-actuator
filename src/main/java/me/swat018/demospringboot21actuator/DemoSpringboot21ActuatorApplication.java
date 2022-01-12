2bd5a0ca2d785359ecda7d8bf26c193fda0185d8
jinwoo park <swat018@gmail.com>
2022-01-12 15:02:20 +0900

    Initial commit

---
+++ src/main/java/me/swat018/demospringboot21actuator/DemoSpringboot21ActuatorApplication.java
@@ -0,0 +1,13 @@
+package me.swat018.demospringboot21actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringboot21ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringboot21ActuatorApplication.class, args);
+    }
+
+}
